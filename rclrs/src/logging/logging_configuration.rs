e9ef5dbc24d4580bf7890381c541e73022b7cb7f
Michael X. Grey <greyxmike@gmail.com>
2024-11-05 20:20:27 +0800

    Putting log handling into its own module
    Signed-off-by: Michael X. Grey <greyxmike@gmail.com>

--- rclrs/src/logging/logging_configuration.rs
+++ rclrs/src/logging/logging_configuration.rs
@@ -1,21 +1,15 @@
-use std::{
-    sync::{Arc, Mutex, OnceLock, Weak},
-    time::Instant,
-};
+use std::sync::{Arc, Mutex, OnceLock, Weak};
 
 use crate::{
     rcl_bindings::*,
-    RclrsError, ToResult, ENTITY_LIFECYCLE_MUTEX, LogSeverity,
+    RclrsError, ToResult, ENTITY_LIFECYCLE_MUTEX,
 };
 
 struct LoggingConfiguration {
     lifecycle: Mutex<Weak<LoggingLifecycle>>,
 }
 
-pub(crate) struct LoggingLifecycle {
-    // Keep the pointer
-    handler: Option<LoggingOutputHandler>,
-}
+pub(crate) struct LoggingLifecycle;
 
 impl LoggingLifecycle {
     fn new(args: &rcl_arguments_t) -> Result<Self, RclrsError> {
@@ -59,40 +53,79 @@ impl Drop for LoggingLifecycle {
     }
 }
 
-pub struct LogLocation<'a> {
-    pub function_name: &'a str,
-    pub file_name: &'a str,
-    pub line_number: usize,
-}
+pub(crate) mod log_handler {
+    //! This module provides a way to customize how log output is handled. For
+    //! now we are not making this a public API and are only using it for tests
+    //! in rclrs. We can consider making it public in the future, but we should
+    //! put more consideration into the API before doing so.
 
-pub type RawLogHandler = Box<dyn Fn(
-    *const rcutils_log_location_t, // location
-    std::os::raw::c_int, // severity
-    *const std::os::raw::c_char, // logger name
-    rcutils_time_point_value_t, // timestamp
-    *const std::os::raw::c_char, // message
-    *mut rcutils_char_array_t, // logging_output
-) + 'static + Send + Sync>;
+    use std::{
+        sync::OnceLock,
+        time::Instant,
+    };
 
-static LOGGING_OUTPUT_HANDLER: OnceLock<RawLogHandler> = OnceLock::new();
+    use crate::{
+        rcl_bindings::*, LogSeverity,
+    };
 
-pub fn set_raw_logging_output_handler(
+    /// Global variable that allows a custom log handler to be set. This log
+    /// handler will be applied throughout the entire application and cannot be
+    /// undone. If you want to be able to change the log handler over the
+    /// lifetime of your application, you should design your own custom handler
+    /// with an Arc<Mutex<T>> inside that allows its behavior to be modified.
+    static LOGGING_OUTPUT_HANDLER: OnceLock<RawLogHandler> = OnceLock::new();
 
-) -> Result<(), RawLogHandler> {
-    Ok(())
-}
+    /// Alias for an arbitrary log handler that is compatible with raw rcl types
+    pub(crate) type RawLogHandler = Box<dyn Fn(
+        *const rcutils_log_location_t, // location
+        std::os::raw::c_int, // severity
+        *const std::os::raw::c_char, // logger name
+        rcutils_time_point_value_t, // timestamp
+        *const std::os::raw::c_char, // message
+        *mut rcutils_char_array_t, // logging_output
+    ) + 'static + Send + Sync>;
 
-pub type LogHandler = Box<dyn Fn(
-    LogLocation,
-    LogSeverity,
-    &str, // logger name
-    Instant, // timestamp
-    &str, // message
+    /// Alias for an abitrary idiomatic log handler
+    pub(crate) type LogHandler = Box<dyn Fn(
+        LogLocation,
+        LogSeverity,
+        &str, // logger name
+        Instant, // timestamp
+        &str, // message
 
-) + 'static + Send + Sync>;
+    ) + 'static + Send + Sync>;
 
-pub fn set_logging_output_handler(
+    /// Rust-idiomatic representation of the location of a log
+    pub(crate) struct LogLocation<'a> {
+        pub function_name: &'a str,
+        pub file_name: &'a str,
+        pub line_number: usize,
+    }
 
-) -> Result<(),  {
+    /// Set an idiomatic log hander
+    pub(crate) fn set_logging_output_handler(
+        handler: LogHandler,
+    ) -> Result<(), RawLogHandler> {
+        let raw_handler = Box::new(
+            |
+                location: *const rcutils_log_location_t,
+                severity: std::os::raw::c_int,
+                logger_name: *const std::os::raw::c_char,
+                timestamp: rcutils_time_point_value_t,
+                message: *const std::os::raw::c_char,
+                logging_output: *mut rcutils_char_array_t,
+            | {
 
+            }
+        );
+
+        set_raw_logging_output_handler(raw_handler)
+    }
+
+    /// Set the raw value for the logging output handler
+    pub(crate) fn set_raw_logging_output_handler(
+        handler: RawLogHandler,
+    ) -> Result<(), RawLogHandler> {
+        LOGGING_OUTPUT_HANDLER.set(handler)
+    }
 }
